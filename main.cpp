dfade434f09d696bf6009c804f48b4ce4edffb9d
Obed <casasolaobed92@gmail.com>
2021-08-05 22:31:44 -0600

    subiendo proyecto 8

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void kilometrosametros(){
+	int kilometros,metros;
+	
+	cout<<"****************************"<<endl;
+	cout<<"*    kilometros a metros   *"<<endl;
+	cout<<"****************************"<<endl;
+	cout<<"cuantos Kilometros desea invertir: ";
+	cin>>kilometros;
+	metros = kilometros * 1000;
+	cout<<kilometros<<" kilometros equivale a: "<<metros<<" metros "<<endl;
+	
+	system("pause");
+	system("cls");
+	}
+	
+	void metrosapulgadas(){
+     int metros,pulgadas;	
+     
+	cout<<"****************************"<<endl;
+	cout<<"*     metros a pulgadas    *"<<endl;
+	cout<<"****************************"<<endl;
+	cout<<"cuantos metros desea invertir: ";
+	cin>>metros;
+	pulgadas = metros * 39.37;
+	cout<<metros<<" metros equivale a: "<<pulgadas<<" pulgadas "<<endl;
+}
+
+int main(int argc, char** argv) {
+	kilometrosametros();
+	metrosapulgadas();
+     
+	return 0;
+}
